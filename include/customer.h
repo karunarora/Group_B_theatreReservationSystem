b4f33256c9721699b02e5e727ce26a26b1adb2ea
Paramveersingh0 <paramveersingh0@gmail.com>
2020-06-22 17:48:02 -0400

    display customers

--- include/customer.h
+++ include/customer.h
@@ -9,15 +9,15 @@
 #include "movie.h"
 
 
-
+int display_customers();
 int add_customer_input();
 int add_customer( char* name,char *username, char sex,char *password);
 int update_customer_input();
 int update_customer( int id, char* name, char sex, char *password);
 int book_movie_input();
 int book_movie(int customer_id, int movie_id, int seat_number);
-
-
+void display_current_customer();
+int display_customer(int id);
 
 float stof(const char* s);
 
